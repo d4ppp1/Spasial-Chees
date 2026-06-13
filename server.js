355e197f7a05464d2d7814827543886a0186cac8
d4ppp1 <d4ppp1@gmail.com>
2026-06-13 20:36:43 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const app = express();
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname)));
+
+// Support both structures: index.html di root ATAU di public/
+const publicDir = fs.existsSync(path.join(__dirname, 'public', 'index.html'))
+  ? path.join(__dirname, 'public')
+  : __dirname;
+
+app.use(express.static(publicDir));
 
 // Explicit fallback untuk root
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(publicDir, 'index.html'));
 });
 
 const games = {};
